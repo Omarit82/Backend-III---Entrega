2bd81520e05370b079ad9d3e8f2f50151be6196d
Omarit82 <123607116+Omarit82@users.noreply.github.com>
2025-06-04 12:51:30 -0300

    Docker

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,5 @@
 import bcrypt from 'bcrypt';
-import {fileURLToPath} from 'url';
-import { dirname } from 'path';
+
 
 export const createHash = async(password) =>{
     const salts = await bcrypt.genSalt(process.env.SALT);
@@ -8,8 +7,3 @@ export const createHash = async(password) =>{
 }
 
 export const passwordValidation = async(user,password) => bcrypt.compare(password,user.password);
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-export default __dirname;
